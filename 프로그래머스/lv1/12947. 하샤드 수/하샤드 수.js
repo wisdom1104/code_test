5f7c4c4287ba13cb9f8ef816ca89ed2213c39060
Jjye_2lrwi <98861355+wisdom1104@users.noreply.github.com>
2023-02-18 15:41:27 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,9 @@
+function solution(x) {
+    var answer = true;
+    let str = String(x);
+    let sum = 0;
+    for (let i=0; i<str.length; i++){
+        sum+= Number(str[i]);
+    }
+    return x % sum == 0 ? true : false;
+}
